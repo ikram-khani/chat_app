02e68a59a71aa81e45aae79321228b1294e3ce6c
Ikram khan <ikr302000@gmail.com>
2023-09-28 22:56:35 +0500

    initial commit of the app

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterChat',
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 63, 17, 177)),
+      ),
+      home: Scaffold(
+        body: Text('Hello Chat APP'),
+      ),
+    );
+  }
+}
