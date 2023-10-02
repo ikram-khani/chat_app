ae543b659c0cadcb22f1bf6924385eb5baafcbd6
Ikram khan <ikr302000@gmail.com>
2023-10-02 21:18:00 +0500

    adding chatMessages and Input widgets

--- lib/view/screens/chat.dart
+++ lib/view/screens/chat.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app/view/widgets/chat_messages.dart';
+import 'package:chat_app/view/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -7,22 +9,25 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("FlutterChat"),
-        actions: [
-          IconButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-            icon: const Icon(Icons.exit_to_app),
-          ),
-        ],
-      ),
-      body: const Center(
-        child: Text(
-          'Logged In!',
+        appBar: AppBar(
+          title: const Text("FlutterChat"),
+          actions: [
+            IconButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              icon: const Icon(Icons.exit_to_app),
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ],
         ),
-      ),
-    );
+        body: const Column(
+          children: [
+            Expanded(
+              child: ChatMessages(),
+            ),
+            NewMessage(),
+          ],
+        ));
   }
 }
